e43d559f711e6f9d090a982d31cfd00c957e340e
Sriram V <vineethsriram7@gmail.com>
2026-08-26 16:58:10 +0530

    Test webhook with corrected trailing slash

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ function add(a, b) {
 console.log('App running (auto-triggered by webhook). 2 + 3 =', add(2, 3));
 module.exports = { add };
 // webhook test Wed Aug 26 09:39:31 AM IST 2026
+// trailing slash fix test
